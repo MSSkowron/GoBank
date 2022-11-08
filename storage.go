9efefa34853561e531d5cdf61f350ef34661ac3c
MSSkowron <mateusz.skowron@poczta.onet.pl>
2022-11-08 17:59:43 +0100

    Finish storage postgres interface implementation

--- storage.go
+++ storage.go
@@ -33,3 +33,19 @@ func NewPostgresStore() (*PostgresStore, error) {
 		db: db,
 	}, nil
 }
+
+func (s *PostgresStore) CreateAccount(a *Account) error {
+	return nil
+}
+
+func (s *PostgresStore) DeleteAccount(id int) error {
+	return nil
+}
+
+func (s *PostgresStore) UpdateAccount(a *Account) error {
+	return nil
+}
+
+func (s *PostgresStore) GetAccountByID(id int) (*Account, error) {
+	return nil, nil
+}
