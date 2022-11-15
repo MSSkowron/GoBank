a03930effef3d2cda347d4711fbe73eef84a2382
MSSkowron <mateusz.skowron@poczta.onet.pl>
2022-11-16 00:30:42 +0100

    Add JWT

--- api.go
+++ api.go
@@ -5,8 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
+	jwt "github.com/golang-jwt/jwt"
 	"github.com/gorilla/mux"
 )
 
@@ -42,7 +44,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
-	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleAccountByID))
+	router.HandleFunc("/account/{id}", withJWTAuth(makeHTTPHandleFunc(s.handleAccountByID), s.storage))
 	router.HandleFunc("/transfer", makeHTTPHandleFunc(s.handleTransfer))
 
 	log.Println("[API] Server running on port:", s.listenAddr[1:])
@@ -85,6 +87,13 @@ func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) 
 	if err := s.storage.CreateAccount(account); err != nil {
 		return err
 	}
+
+	tokenString, err := createJWT(account)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("JWT TOKEN: ", tokenString)
 
 	return WriteJSON(w, http.StatusOK, account)
 }
@@ -170,5 +179,67 @@ func getIDFromRequest(r *http.Request) (int, error) {
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
+
 	return json.NewEncoder(w).Encode(v)
+}
+
+func createJWT(account *Account) (string, error) {
+	claims := &jwt.MapClaims{
+		"expiresAt":     15000,
+		"accountNumber": account.Number,
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(secret))
+}
+
+func withJWTAuth(handlerFunc http.HandlerFunc, s Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("calling JWT auth middleware")
+
+		tokenString := r.Header.Get("x-jwt-token")
+
+		token, err := validateJWT(tokenString)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+			return
+		}
+		if !token.Valid {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+			return
+		}
+
+		userID, err := getIDFromRequest(r)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+			return
+		}
+
+		account, err := s.GetAccountByID(userID)
+		if err != nil {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+			return
+		}
+
+		if account.Number != int64(token.Claims.(jwt.MapClaims)["accountNumber"].(float64)) {
+			WriteJSON(w, http.StatusForbidden, APIError{Error: "permission denied"})
+			return
+		}
+
+		handlerFunc(w, r)
+	}
+}
+
+func validateJWT(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("JWT_SECRET")
+
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(secret), nil
+	})
 }
